bbed79b764a11c726240319426feefa3dd0b93bf
Andrew <wrege511@gmail.com>
2020-10-05 23:15:05 -0500

    Clean up usings, add required property, remove template files.

--- Models/Email.cs
+++ Models/Email.cs
@@ -1,25 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
 
-namespace UniqueEmailService
+namespace UniqueEmailService.Models
 {
     public class Email
     {
         //list of domains that support account matching
         private static readonly IEnumerable<string> DomainSupportsMatching = new HashSet<string> { "gmail.com" };
         private string _rawEmail;
-        private string _uniqueEmail;
         private const string ACC_MATCH_IGNORE_AFTER = "+";
         private const string ACC_MATCH_IGNORE_CHAR = ".";
+        private const string AT_SYM = "@";
 
-        public Email(string email)
+        [EmailAddress]
+        [Required]
+        public string RawEmail
         {
-            RawEmail = email;
-        }
-
-        public string RawEmail { 
             get
             {
                 return _rawEmail;
@@ -27,7 +25,7 @@ namespace UniqueEmailService
             set
             {
                 _rawEmail = value;
-                _uniqueEmail = GetUniqueEmail();
+                UniqueEmail = GetUniqueEmail();
             }
         }
 
@@ -38,26 +36,22 @@ namespace UniqueEmailService
                 //Per RFC 1035 3.1, domains must be compared case insensitive, so convert to lower case.
                 //Per RFC 5321 2.4 Mailbox local parts (before @) are case sensitive (despite implementations that ignore this) and so it is dangerous to
                 //  check for uniqueness ignoring case on the entire email address.
-                return RawEmail.Substring(RawEmail.IndexOf('@'), RawEmail.Length).ToLower();
+                return RawEmail.Substring(RawEmail.IndexOf('@') + 1).ToLower();
             }
         }
 
-        public string UniqueEmail {
-            get 
-            {
-                return _uniqueEmail;
-            }
-        }
+        public string UniqueEmail { get; private set; }
 
-        private string GetUniqueEmail() {
+        private string GetUniqueEmail()
+        {
             string uniqueEmail;
-            if (Email.DomainSupportsMatching.Contains(Domain))
+            if (DomainSupportsMatching.Contains(Domain))
             {
-                string localPart = RawEmail.Substring(0, RawEmail.IndexOf('@'));
-                localPart = localPart.Substring(0, localPart.IndexOf(ACC_MATCH_IGNORE_AFTER));
-                localPart = localPart.Replace(ACC_MATCH_IGNORE_CHAR, String.Empty);
+                string localPart = RawEmail.Split(AT_SYM).First();
+                localPart = localPart.Split(ACC_MATCH_IGNORE_AFTER).First();
+                localPart = localPart.Replace(ACC_MATCH_IGNORE_CHAR, string.Empty);
 
-                uniqueEmail = localPart + Domain;
+                uniqueEmail = localPart + '@' + Domain;
             }
             else
             {
